d2b5bf5b5bce395fc09c2e409b792b48414018b0
alemenew <alemenewendawek@gmail.com>
2022-08-29 23:53:03 +0300

    print

---
+++ python/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = (word[:2])
+word_last_2 = (word[]
+middle_word = []
+print(f"first 3 leatters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
